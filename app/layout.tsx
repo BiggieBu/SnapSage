6c7685f44a4080e3f52e59b23444b23772a02976
BiggieBu <parvyoy@gmail.com>
2024-07-12 12:26:47 +0530

    Routes and Auth Set. Also resolved the infinite redirect caused by redirecting the sign in and sign up routes

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Kadwa } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const kadwa = Kadwa({
   subsets: ["latin"],
@@ -20,10 +21,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={cn("font-kadwa antialiased", kadwa.variable)}>
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={cn("font-kadwa antialiased", kadwa.variable)}>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
